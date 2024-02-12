a0f40f37b52e04ebd9223766c2de42857e6e0b4c
Xinyue Wang <102383375+mia0204@users.noreply.github.com>
2024-02-12 00:31:20 -0400

    Added layers on the web map

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ map.on('load', () => {
         }
     });
 
-    //draw layers on the map
+    //draw layer on the map
     map.addLayer({
         'id': 'uoft-pnt', //unique layer name
         'type': 'circle',
@@ -44,5 +44,42 @@ map.on('load', () => {
             'circle-color': '#B42222'
         }
     });
+
+    //add a data source from a GeoJSON file
+    map.addSource('buildings-data', {
+        type: 'geojson',
+        //raw link in github to the geojson file
+        data: 'https://raw.githubusercontent.com/mia0204/wk5-exercise/main/buildings.geojson'
+    });
+
+    map.addLayer({
+        'id': 'buidlings-point',
+        'type': 'circle',
+        'source': 'buildings-data',
+        'paint': {
+            'circle-radius': 5,
+            'circle-color': '#007cbf'
+        }
+    });
+
+    //add a data source from a Mapbox tileset
+    map.addSource('toronto-ct', {
+        'type': 'vector',
+        'url': 'mapbox://wxyue0204.ciowcz6b' //mapbox tileset ID
+    });
+
+    map.addLayer({
+        'id': 'toronto-census-tract',
+        'type': 'fill',
+        'source': 'toronto-ct',
+        'paint': {
+            'fill-color': '#888888',
+            'fill-opacity': 0.4,
+            'fill-outline-color': 'purple'
+        },
+        'source-layer': 'torontoct-9a1gfe' //Mapbox tileset name
+    },
+        //'uoft-buildings' //drawing order
+    );
 
 });
